86a57fbd02b268f9a9a0c21c9a3a21c3bba389b6
Brenda David <brenda.silveira@dbserver.com>
2023-07-14 15:39:32 -0300

    adiciona modulo e rota para healtCheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ChallengesModule } from './challenges/challenges.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ChallengesModule],
+  imports: [ConfigModule.forRoot(), ChallengesModule, HealthModule],
 })
 export class AppModule {}
